8e5e11df06891c3ebee9376cd2f90294f5da2944
yash <yashraj0207@outlook.com>
2022-12-31 17:40:18 +0530

    Initial setup

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import { Montserrat } from '@next/font/google'
+import type { AppProps } from 'next/app'
+
+import '../styles/globals.css'
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  variable: '--font-montserrat',
+})
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <div className={`${montserrat.variable} font-sans`}>
+      <Component {...pageProps} />
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['var(--font-montserrat)'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
